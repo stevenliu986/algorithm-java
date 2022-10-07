ab07e3f4036019664def189dd66b2dc0e13a64f1
StevenLiu <stevenliu986@gmail.com>
2022-10-07 18:39:42 +1100

    添加冒泡排序的原理说明

--- Code04_BubbleSort.java
+++ Code04_BubbleSort.java
@@ -1,5 +1,11 @@
 import java.util.Arrays;
 
+/**
+ * 冒泡排序
+ * 原理：将前一位的数与后面的数进行比较，如果前面的数比后面的数大，两两交换，直到将
+ * 最大的数放到最后，然后数组的长度减一，以此类推
+ */
+
 public class Code04_BubbleSort {
     public static void main(String[] args) {
         int[] arr01 = {2, 5, 90, 4, -1, 45, 34, 23};
